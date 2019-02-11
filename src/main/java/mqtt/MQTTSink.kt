bafb57d03ab82bf4f6084ce00848747006eb082e
Max Berndt <max.berndt@iteratec.de>
2019-02-11 13:24:06 +0100

    reconnect to broker on connection lost

--- src/main/java/mqtt/MQTTSink.kt
+++ src/main/java/mqtt/MQTTSink.kt
@@ -7,6 +7,10 @@ import org.eclipse.paho.client.mqttv3.*
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence
 import org.slf4j.LoggerFactory
 import traze.PlayerAggregationResult
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken
+import org.eclipse.paho.client.mqttv3.MqttCallback
+
+
 
 class MQTTSink(config: MQTTSinkConfig) : RichSinkFunction<PlayerAggregationResult>(), MqttCallback {
 
@@ -17,9 +21,9 @@ class MQTTSink(config: MQTTSinkConfig) : RichSinkFunction<PlayerAggregationResul
     private val topic: String = config.topicArg
     private val qos: Int = config.qosArg
     private val retained: Boolean = config.retainedArg
+    private var callback: MqttCallback? = null
 
-    override fun open(parameters: Configuration) {
-        super.open(parameters)
+    private fun connect() {
         val connectOptions = MqttConnectOptions()
         connectOptions.isCleanSession = true
 
@@ -31,17 +35,35 @@ class MQTTSink(config: MQTTSinkConfig) : RichSinkFunction<PlayerAggregationResul
         client = MqttClient(brokerAddress, genClientId, MemoryPersistence())
 
         client?.connect(connectOptions)
-        client?.setCallback(this)
+        client?.setCallback(callback)
 
         log.info("connected to mqtt broker $brokerAddress from sink")
     }
 
+    override fun open(parameters: Configuration) {
+        super.open(parameters)
+        callback = object : MqttCallback {
+            override fun connectionLost(t: Throwable) {
+                this@MQTTSink.connect()
+            }
+
+            @Throws(Exception::class)
+            override fun messageArrived(topic: String, message: MqttMessage) {}
+            override fun deliveryComplete(token: IMqttDeliveryToken) {}
+        }
+
+        connect()
+    }
+
     override fun invoke(result: PlayerAggregationResult, context: SinkFunction.Context<*>) {
         try {
+            if (!client!!.isConnected) {
+                throw Exception("MqttClient is not connected")
+            }
             val bytes = result.toString().toByteArray(Charsets.UTF_8)
             log.info("Publishing ${bytes.toString(Charsets.UTF_8)} to $topic")
             client?.publish(topic, bytes, qos, retained)
-        } catch (e: MqttException) {
+        } catch (e: Exception) {
             throw RuntimeException("Failed to publish", e)
         }
     }
